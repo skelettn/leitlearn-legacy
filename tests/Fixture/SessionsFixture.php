934d6da92863263784e0e3baed773394d44abafe
Léo Trux <leotrux@Mac-mini-de-Leo.local>
2024-03-19 19:42:10 +0100

    ajout table sessions

---
+++ tests/Fixture/SessionsFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * SessionsFixture
+ */
+class SessionsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'packet_id' => 1,
+                'created' => '2024-03-19 18:41:44',
+                'modified' => '2024-03-19 18:41:44',
+            ],
+        ];
+        parent::init();
+    }
+}
