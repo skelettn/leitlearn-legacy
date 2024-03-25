88baf705fe823ad4b37a62153dbe8f0f543dace1
Aiman Manchout <magicaiman@MacBook-Air-de-Aiman.local>
2024-03-25 21:51:02 +0100

    avancement de la traduction jusqu'a login

--- templates/element/modals/create_flashcard.php
+++ templates/element/modals/create_flashcard.php
@@ -1,7 +1,7 @@
 <div class="modal" id="create-flashcard">
     <div class="modal-container wider">
         <div class="modal-header">
-            <h2 class="title">Créer une flashcard</h2>
+            <h2 class="title"><?= __('Créer une flashcard') ?></h2>
             <div class="modal-close">
                 <span class="material-symbols-rounded">
                     close
@@ -11,17 +11,17 @@
         <div class="modal-body">
             <?php echo $this->Form->create(null, ['url' => ['controller' => 'Flashcards', 'action' => 'create', $packet->id]]); ?>
             <div class="input-group front">
-                <span class="title">Recto de la carte</span>
+                <span class="title"><?= __('Recto de la carte') ?></span>
                 <div class="text-area" id="editor-flashcard-front"></div>
                 <?= $this->Form->control('question', ['id' => 'question', 'type'=>'hidden']) ?>
             </div>
             <div class="input-group back">
-                <span class="title">Recto de la carte</span>
+                <span class="title"><?= __('Verso de la carte') ?></span>
                 <div class="text-area" id="editor-flashcard-back"></div>
                 <?= $this->Form->control('answer', ['id' => 'answer', 'type'=>'hidden']) ?>
             </div>
             <div class="loader-button">
-                <?= $this->Form->submit('Créer') ?>
+                <?= $this->Form->submit(__('Créer')) ?>
                 <span class="loader"></span>
             </div>
             <?= $this->Form->end() ?>
--- templates/element/modals/import_packet.php
+++ templates/element/modals/import_packet.php
@@ -1,7 +1,7 @@
 <div class="modal" id="import-packet">
     <div class="modal-container">
         <div class="modal-header">
-            <h2 class="title">Importer un paquet</h2>
+            <h2 class="title"><?= __('Importer un paquet') ?></h2>
             <div class="modal-close">
                 <span class="material-symbols-rounded">close</span>
             </div>
@@ -10,11 +10,11 @@
             <?= $this->Form->create(null, ['url' => ['controller' => 'Packets', 'action' => 'importViaFile'], 'enctype' => 'multipart/form-data']) ?>
             <div class="input-group">
                 <?= $this->Form->text('name', ['placeholder' => '', 'required' => true]) ?>
-                <?= $this->Form->label('name', 'Nom du paquet') ?>
+                <?= $this->Form->label('name', __('Nom du paquet')) ?>
             </div>
             <div class="input-group">
                 <?= $this->Form->text('description', ['placeholder' => '', 'required' => true]) ?>
-                <?= $this->Form->label('description', 'Description du paquet') ?>
+                <?= $this->Form->label('description', __('Description du paquet')) ?>
             </div>
             <div class="leitlearn_import_paquet_via_file">
                 <label for="uploaded_packet">
@@ -23,14 +23,14 @@
                             upload_file
                         </span>
                     </div>
-                    <h3 class="title">Parcourir les fichiers</h3>
-                    <span class="supported">Types de fichiers pris en charge : .csv, .apkg (Anki)</span>
+                    <h3 class="title"><?= __('Parcourir les fichiers') ?></h3>
+                    <span class="supported"><?= __('Types de fichiers pris en charge : .csv, .apkg (Anki)') ?></span>
                 </label>
                 <?= $this->Form->file('uploaded_file', ['name' => 'uploaded_file', 'id' => 'uploaded_packet', 'accept' => '.csv, .apkg']) ?>
             </div>
             <?= $this->Form->hidden('ia', ['value' => 0]); ?>
             <div class="loader-button">
-                <?= $this->Form->submit('Importer le paquet') ?>
+                <?= $this->Form->submit(__('Importer le paquet')) ?>
                 <span class="loader"></span>
             </div>
             <?= $this->Form->end() ?>
