aea03ff661550b061119fc668779577762c9fc79
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-03-31 16:45:50 +0200

    bundle.js prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './webpack/index.js', // Point d'entrée de votre application JavaScript
-    mode: 'development', // ou 'production'
+    mode: 'production', // ou 'production'
     output: {
         path: path.resolve(__dirname, 'webroot/js'), // Répertoire de sortie dans webroot/js
         filename: 'bundle.js' // Nom du fichier de sortie
