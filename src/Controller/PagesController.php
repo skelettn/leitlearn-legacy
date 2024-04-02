1e965c44b5fc0efb85bd94e9b3cce9cdcba8c46e
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-04-02 14:57:45 +0200

    graphic issues on iphone

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -19,6 +19,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Cake\Core\Configure;
+use Cake\Event\EventInterface;
 use Cake\Http\Exception\ForbiddenException;
 use Cake\Http\Exception\NotFoundException;
 use Cake\Http\Response;
@@ -33,6 +34,12 @@ use Cake\View\Exception\MissingTemplateException;
  */
 class PagesController extends AppController
 {
+    public function beforeFilter(EventInterface $event)
+    {
+        parent::beforeFilter($event);
+
+        $this->Authentication->allowUnauthenticated(['display']);
+    }
     /**
      * Displays a view
      *
